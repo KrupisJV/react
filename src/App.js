73d9ef027544a682db01e358459ef5456bbaaaef
ipa21.j.vintermanis <ipa21.j.vintermanis@pjvt.local>
2023-10-23 16:51:53 +0300

    updeit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Counter from "./Counter";
 
 function App() {
+  const masivs = [
+    "skatitajs",
+    "Hellcat",
+    "Niggers",
+    "bomars",
+  ];
+
+  const masivsJSX = masivs.map(entry => {
+    return <Counter name={entry} key={entry} />
+  })
   return (
     <>
       <h1>Hi mom</h1>
-      <Counter />
+      {masivsJSX}
     </>
   );
 }
